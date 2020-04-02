7b5bafbefeffb33f140116815cda192b43b87ac8
Raxa Hansana <raxah@Raxas-MacBook-Pro.local>
2020-04-02 15:53:18 -0700

    Completed step 5 - added methods to index.js

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const massive = require('massive');
 const app = express();
 const { SERVER_PORT, CONNECTION_STRING } = process.env;
 
+const products_controller = require('./products_controller');
+
 massive({
   connectionString: CONNECTION_STRING,
   ssl: { rejectUnauthorized: false },
@@ -14,6 +16,12 @@ massive({
     app.set('db', db);
   })
   .catch(err => console.log(err));
+
+app.get('/api/products', products_controller.getAll);
+app.get('/api/products/:id', products_controller.getOne);
+app.put('/api/products/:id', products_controller.update);
+app.post('/api/products', products_controller.create);
+app.delete('/api/products/:id', products_controller.delete);
 
 app.use(express.json());
 app.listen(SERVER_PORT, () => {
